7a8c8fcd26ad52f278b7beb713d2f8509eb48dd2
Sergiy Tsybulko <sergiy@futurenet.club>
2017-08-01 14:39:00 +0000

    add paths to config

--- config/wbt.php
+++ config/wbt.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-    'api_key' => env('WBT_API_KEY')
+    'api_key' => env('WBT_API_KEY'),
+    
+    'locale' => '',
+    
+    'lang_paths' => [],
+    
+    'group_delimiter' => '::',
 ];
