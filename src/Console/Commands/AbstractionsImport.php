6d2fcdf053edd581b4be36590702c5ba4a93db4f
Yura <yuriy.shnitsar@gmail.com>
2017-08-01 17:51:57 +0300

    add artisan command abstractions:import

--- src/Console/Commands/AbstractionsImport.php
+++ src/Console/Commands/AbstractionsImport.php
@@ -5,14 +5,14 @@ namespace WBTranslator\PluginLaravel\Console\Commands;
 use Illuminate\Console\Command;
 use WBTranslator\PluginLaravel\Http\Controllers\WBTranslatorController;
 
-class AbstractionsExport extends Command
+class AbstractionsImport extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'abstractions:export';
+    protected $signature = 'abstractions:import';
 
     /**
      * The console command description.
@@ -45,7 +45,7 @@ class AbstractionsExport extends Command
         $this->info('Process ... ');
 //        $bar = $this->output->createProgressBar();
 
-        $response = $this->controller->export();
+        $response = $this->controller->import();
 
 //        $bar->finish();
         $this->info($response->getContent());
--- src/Providers/WBTranslatorServiceProvider.php
+++ src/Providers/WBTranslatorServiceProvider.php
@@ -5,6 +5,7 @@ namespace WBTranslator\PluginLaravel\Providers;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use WBTranslator\PluginLaravel\Console\Commands\AbstractionsExport;
+use WBTranslator\PluginLaravel\Console\Commands\AbstractionsImport;
 
 /**
  * Class WBTranslatorServiceProvider
@@ -21,7 +22,8 @@ class WBTranslatorServiceProvider extends ServiceProvider
         ];
 
         $this->commands([
-            AbstractionsExport::class
+            AbstractionsExport::class,
+            AbstractionsImport::class
         ]);
 
         $this->publishes($paths, 'config');
